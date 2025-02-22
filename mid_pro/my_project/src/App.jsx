2c640cedbad36f0a2cf41d0c06af63bbbc37efa9
Saidmurod <s.xamidov@newuu.uz>
2025-02-22 16:58:32 +0500

    asdf

--- mid_pro/my_project/src/App.jsx
+++ mid_pro/my_project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import img from "./assets/react.svg"
-import ChefsCard from "./AboutDev";
+import ChefsCard from "./AboutCooks";
+import DevelopersCard from "./AboutDevs";
 const App = () => {
     return (
         <div className="container mt-5 d-flex justify-content-center">
-           <ChefsCard/>
+           <DevelopersCard/>
         </div>
     );
 };
