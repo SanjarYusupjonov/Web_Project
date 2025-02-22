441ec767766293906d54cc666d80ab7f5ff3a89a
sogdiiiana <s.ravshanova@newuu.uz>
2025-02-22 15:07:04 +0300

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -4,13 +4,20 @@ import "./contact.css";
 function Contact() {
   return (
     <div className="contact-container">
-      <h2 className="contact-title">CONTACTS - NEW UZBEKISTAN CANTEEN</h2>
+      <div className="event-card">
+        <div className="image-container">
+          <img src="/canteen.png" alt="Canteen" />
+        </div>
 
-      <p>Do you have any questions?!</p>
-      <p>Feel free to come and ask them personally 😊</p>
-
-      <p>Daily working hours: <strong>09:00 AM - 6:00 PM</strong>.</p>
-
+        <div className="text-container">
+          <h2 className="contact-title"> NEW UZBEKISTAN CANTEEN</h2>
+          <p>Do you have any questions?!</p>
+          <p>Feel free to come and ask them personally 😊</p>
+          <p>
+            Daily working hours: <strong>09:00 AM - 6:00 PM</strong>.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
