f8537c34cbe89830f0fbb272d0f1978fb19c814e
ibadlayevam19102005 <m.ibadlayeva@newuu.uz>
2025-02-24 01:27:20 +0500

    general design for blog page

--- Blog.js
+++ Blog.js
@@ -6,42 +6,42 @@ const blogPosts = [
     title: "🍽️ New Canteen Menu Update!",
     content:
       "We’ve added fresh salads, fruit bowls, and vegan-friendly meals. Try our new Quinoa Salad today!",
-    image: "https://source.unsplash.com/300x200/?salad,healthy",
+    image: "https://i.pinimg.com/736x/a2/3f/34/a23f34a3a2a3bc1f5bfc951ae722e2b3.jpg",
   },
   {
     id: 2,
-    title: "🔥 Student Favorite: Spicy Chicken Wrap",
+    title: "🔥 Student Favorite: Manti",
     content:
-      "Our famous spicy chicken wrap is now the most popular dish in the canteen! Have you tried it yet?",
-    image: "https://source.unsplash.com/300x200/?chicken,wrap",
+      "Our manti is now the most popular dish in the canteen! Have you tried it yet?",
+    image: "https://i.pinimg.com/736x/6e/2b/13/6e2b13db4d8093fd15ace1b21074f029.jpg",
   },
   {
     id: 3,
     title: "💰 Get 20% Off on Fridays!",
     content:
       "Every Friday, enjoy a 20% discount on all main meals when you show your student ID.",
-    image: "https://source.unsplash.com/300x200/?discount,food",
+    image: "https://i.pinimg.com/736x/31/98/56/319856ae89374e02341a782e08f64310.jpg",
   },
   {
     id: 4,
     title: "🌍 Eco-Friendly Initiative: Bring Your Own Cup!",
     content:
       "We’re reducing waste! Bring your own reusable cup and get a discount on all hot drinks.",
-    image: "https://source.unsplash.com/300x200/?coffee,cup",
+    image: "https://i.pinimg.com/736x/c0/47/55/c047550514b572161c31191d84a7c148.jpg",
   },
   {
     id: 5,
-    title: "🏆 Cooking Competition – Join Now!",
+    title: "🏆 Ramadan charity – Join Now!",
     content:
-      "Think you’re a great cook? Show off your skills in our student cooking competition next week!",
-    image: "https://source.unsplash.com/300x200/?cooking,competition",
+      "You want Ramadan wibe and dua’? Show off your skills in preparing iftaar and share with people !",
+    image: "https://i.pinimg.com/736x/76/33/ef/7633efdbd95dfa985347777379ee8f69.jpg",
   },
   {
     id: 6,
     title: "📢 Your Feedback Matters!",
     content:
       "Tell us what new meals you’d like to see on the menu. Vote in our monthly canteen survey!",
-    image: "https://source.unsplash.com/300x200/?feedback,survey",
+    image: "https://i.pinimg.com/736x/3a/91/95/3a9195ed78e260c9cde86c9bfc3e7717.jpg",
   },
 ];
 
@@ -73,4 +73,5 @@ function Blog() {
 }
 
 export default Blog;
+
 
